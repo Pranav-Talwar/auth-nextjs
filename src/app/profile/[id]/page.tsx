c821c756adf8b119d34f2b19b5b348f112f93fa0
user.email <talwarpranav929@gmail.com>
2025-05-24 09:52:09 -0600

    Add user model and  profile page

---
+++ src/app/profile/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+
+export default function UserProfile({ params }: any) {
+    const { id } = React.use(params);
+
+    return (
+        <div className="flex flex-col items-center p-6 justify-center min-h-screen ">
+            <h1>Profile</h1>
+            <hr />
+            <p className='text-4xl'>Profile Page: {id} </p>
+        </div>
+    );
+}
